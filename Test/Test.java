bf8887fb1b1eb9e0dbc40dafcb92ddfd821c50eb
Kevin Tong <kcr1230@hotmail.com>
2016-01-06 19:06:45 -0800

    Test

--- Test/Test.java
+++ Test/Test.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        char x = 66;
+        checkNumsPretty(1);
+        checkNumsPretty(1);
+    }
 
-        System.out.println(x);
+    public static void checkNumsPretty(int max) {
+
+//        Scanner sc = new Scanner(System.in);
+//        int num;
+//
+//        System.out.println("Please enter a number.");
+//        while (!sc.hasNextInt()) {
+//            sc.next();
+//            System.out.println("Invalid input. Please enter a valid number.");
+//        }
+//        num = sc.nextInt();
+
+        int num = 1001;
+        int firstNum = Integer.parseInt(Integer.toString(num).substring(0));
+
+        int last = num % 10;
+
+        if (firstNum == last) {
+            System.out.println("pretty");
+        } else {
+            System.out.println(num);
+        }
     }
 }
