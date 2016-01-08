46a798121272c26ddd3f99a85c58ab8a56e0af8d
Kevin Tong <kcr1230@hotmail.com>
2016-01-08 00:38:51 -0800

    Various things

--- Test/Test2.java
+++ Test/Test2.java
@@ -1,11 +1,11 @@
 public class Test2 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int number = 534;
-        int firstDigit = Integer.parseInt(Integer.toString(number).substring(0, 1));
+		int number = 534;
+		int firstDigit = Integer.parseInt(Integer.toString(number).substring(0, 1));
 
-        System.out.println(firstDigit);
+		System.out.println(firstDigit);
 
-    }
+	}
 }
--- Week 1/Quiz 1 Problem 3/q1p3/Q1P3.java
+++ Week 1/Quiz 1 Problem 3/q1p3/Q1P3.java
@@ -2,29 +2,29 @@ package q1p3;
 
 public class Q1P3 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("First iteration:");
-        checkNumsPretty(101);
-        System.out.println("Second iteration:");
-        checkNumsPretty(102);
-        System.out.println("Third iteration:");
-        checkNumsPretty(201);
-        System.out.println("Fourth iteration:");
-        checkNumsPretty(202);
-    }
+		System.out.println("First iteration:");
+		checkNumsPretty(101);
+		System.out.println("Second iteration:");
+		checkNumsPretty(102);
+		System.out.println("Third iteration:");
+		checkNumsPretty(201);
+		System.out.println("Fourth iteration:");
+		checkNumsPretty(202);
+	}
 
-    public static void checkNumsPretty(int max) {
+	public static void checkNumsPretty(int max) {
 
-        for (int i = 1; i <= max; i++) {
-            int firstNum = Integer.parseInt(Integer.toString(i).substring(0, 1));
-            int lastNum = i % 10;
+		for (int i = 1; i <= max; i++) {
+			int firstNum = Integer.parseInt(Integer.toString(i).substring(0, 1));
+			int lastNum = i % 10;
 
-            if (firstNum == lastNum) {
-                System.out.println("\tpretty");
-            } else {
-                System.out.println("\t" + i);
-            }
-        }
-    }
+			if (firstNum == lastNum) {
+				System.out.println("\tpretty");
+			} else {
+				System.out.println("\t" + i);
+			}
+		}
+	}
 }
