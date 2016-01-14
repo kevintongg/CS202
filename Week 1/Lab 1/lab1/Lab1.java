7834a945384d67ddea5d1d56be3da90b008fb5e7
Kevin Tong <kcr1230@hotmail.com>
2016-01-14 00:55:54 -0800

    Lab 1 Update

--- Week 1/Lab 1/lab1/Lab1.java
+++ Week 1/Lab 1/lab1/Lab1.java
@@ -1,20 +1,25 @@
-package test;
+package lab1;
 
 import java.util.Scanner;
 
-public class Test {
+public class Lab1 {
 
 	static Scanner sc = new Scanner(System.in);
-	static int rows = 4;
+	static int rows = 5;
 	static int columns = 5;
-	static char[][] table = new char[rows][columns];
-
+	static String[][] table = new String[rows][columns];
+	static int basketballStudents = 0;
+	static int swimStudents = 0;
+	static int pingPongStudents = 0;
+	static int badmintonStudents = 0;
+	static int yogaStudents = 0;
 
 	public static void main(String[] args) {
 
-		welcome();
+		System.out.println("\nWelcome to Gym Class Scheduling!");
 		makeTable();
-		schedule();
+		tableHeaders();
+		pickOption();
 
 	}
 
@@ -22,50 +27,37 @@ public class Test {
 
 		for (int i = 0; i < table.length; i++) {
 			for (int j = 0; j < table[i].length; j++) {
-				table[i][j] = ' ';
+				table[i][j] = "";
 			}
 		}
 	}
 
-	public static void printTable(char[][] table) {
+	public static void printTable(String[][] table) {
 
-		for (char[] i : table) {
-			System.out.println("\t=================================");
-			for (char j : i) {
-				System.out.print("\t|" + j);
+		System.out.println("The first column is Monday, second Thursday, third Friday, and fourth Saturday.\n");
+		System.out.println("The first row is 9 to 10 AM, second 1 to 2 PM, third 3 to 4 PM, and fourth 4 to 5 PM");
+
+		for (String[] i : table) {
+			System.out.println("\t=======================================================================================================");
+			for (String j : i) {
+				System.out.print("\t|" + "\t" + j);
 			}
 			System.out.println();
 		}
 	}
 
-	public static void welcome() {
+	public static void tableHeaders() {
 
-		System.out.println("\nWelcome to Gym Class Scheduling!\n");
-	}
+		table[0][0] = "Gym Schedule";
+		table[0][1] = "M";
+		table[0][2] = "R";
+		table[0][3] = "F";
+		table[0][4] = "S";
+		table[1][0] = "9 to 10 AM";
+		table[2][0] = "1 to 2 PM";
+		table[3][0] = "3 to 4 PM";
+		table[4][0] = "4 to 5 PM";
 
-	public static void pickOption() {
-
-		int choice = 0;
-
-		while (choice != 0) {
-			System.out.println(
-					"Enter '0' to quit, '1' to show the current schedule, '2' to schedule a class, and '3' to add students.");
-			choice = sc.nextInt();
-			switch (choice) {
-				case 1:
-					System.out.println("The current schedule is:");
-					printTable(table);
-					break;
-				case 2:
-					System.out.println("We will now schedule a class.");
-					schedule();
-					break;
-				case 3:
-					System.out.println("We will add a student to a specified class.");
-					addStudent();
-					break;
-			}
-		}
 	}
 
 	public static void rowInput() {
@@ -97,89 +89,157 @@ public class Test {
 			}
 			columns = sc.nextInt() - 1;
 			if (columns < 0 || columns > 3) {
-				System.out.println("A column number between 1 and 3, please!");
+				System.out.println("A column number between 1 and 4, please!");
 			}
 
 		} while (rows < 0 || rows > 3);
 	}
 
-	public static void addClassToSchedule(char[] table) {
+	public static void pickOption() {
 
-		final String[] classes = { "Basketball Class, Thursdays only, 1:00 to 2:00 PM, Maximum Students: 20",
-				"Swim Class, Fridays and Saturdays, 1:00 to 2:00 PM, Students: 10",
-				"Ping Pong Class, Mondays only, 9:00 to 10:00AM, Maximum Students: 10",
-				"Badminton Class, Mondays only, 1:00 to 2:00, Maximum Students: 12",
-				"Yoga Class, MRFS, 4:00 to 5:00 PM, Maximum Students: 15" };
+		int choice = 0;
 
-//		if (rows == 0 && )
+		do {
+			System.out.println(
+					"\nEnter '0' to quit, '1' to show the current schedule, '2' to schedule a class, and '3' to add students.");
+			while (!sc.hasNextInt()) {
+				sc.nextLine();
+				System.out.println(
+						"Invalid input! Please enter '0' to quit, '1' to show the current schedule, '2' to schedule a class, and '3' to add students.");
+			}
+			choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					System.out.println("The current schedule is:");
+					printTable(table);
+					break;
+				case 2:
+					System.out.println("We will now schedule a class.");
+					scheduling();
+					break;
+				case 3:
+					System.out.println("We will add a student to a specified class.");
+					addStudent();
+					break;
+			}
+			if (choice < 0 || choice > 3) {
+				System.out.println(
+						"Invalid input! Please enter '0' to quit, '1' to show the current schedule, '2' to schedule a class, and '3' to add students.");
+			}
+		} while (choice != 0);
 	}
 
 	public static void addStudent() {
 
 		int choice = 0;
-		int basketballStudents = 0;
-		int swimStudents = 0;
-		int pingPongStudents = 0;
-		int badmintonStudents = 0;
-		int yogaStudents = 0;
-		String yes;
 
-		System.out.println(
-				"Class #1: Basketball\nClass #2: Swim\nClass #3: Ping Pong\nClass #4: Badminton\nClass #5: Yoga");
 
 		do {
+			System.out.println(
+					"Enter one of the following options:\n'0' to quit this part of the program\n'1' to add students to Basketball Class\n'2' to add students to Swim Class\n'3' to add students to Ping Pong Class\n'4' to add students to Badminton Class\n'5' to add students to Yoga Class");
+			while (!sc.hasNextInt()) {
+				sc.nextLine();
+				System.out.println(
+						"Invalid input! Enter one of the following options:\n'0' to quit this part of the program\n'1' to add students to Basketball Class\n'2' to add students to Swim Class\n'3' to add students to Ping Pong Class\n'4' to add students to Badminton Class\n'5' to add students to Yoga Class");
+			}
+			choice = sc.nextInt();
 			switch (choice) {
 				case 1:
-					System.out.println("Do you want to add students to the basketball class? (Y/N)");
-					yes = sc.nextLine().toLowerCase();
-					if (yes == "y" && basketballStudents <= 20) {
+					if (basketballStudents < 20) {
 						basketballStudents++;
+						System.out.println("Current students in the basketball class: " + basketballStudents);
+					} else {
+						System.out.println("Sorry, this class has reached the maximum number of students allowed.");
+						addStudent();
 					}
-					addStudent();
 					break;
 				case 2:
-					System.out.println("Do you want to add students to the swim class? (Y/N)");
-					yes = sc.nextLine().toLowerCase();
-					if (yes == "y" && swimStudents <= 10) {
+					if (swimStudents < 10) {
 						swimStudents++;
+						System.out.println("Current students in the swim class: " + swimStudents);
+					} else {
+						System.out.println("Sorry, this class has reached the maximum number of students allowed.");
+						addStudent();
 					}
 					break;
 				case 3:
-					System.out.println("Do you want to add students to the Ping Pong class? (Y/N)");
-					yes = sc.nextLine().toLowerCase();
-					if (yes == "y" && pingPongStudents <= 10) {
+					if (pingPongStudents < 10) {
 						pingPongStudents++;
-					} else if (pingPongStudents == 10) {
-						System.out.println("Sorry! Class full!");
+						System.out.println("Current students in the ping pong class: " + pingPongStudents);
+					} else {
+						System.out.println("Sorry, this class has reached the maximum number of students allowed.");
+						addStudent();
 					}
 					break;
 				case 4:
-					System.out.println("Do you want to add students to the Badminton class? (Y/N)");
-					yes = sc.nextLine().toLowerCase();
-					if (yes == "y" && badmintonStudents <= 12) {
+					if (badmintonStudents < 12) {
 						badmintonStudents++;
+						System.out.println("Current students in the badminton class: " + badmintonStudents);
+					} else {
+						System.out.println("Sorry, this class has reached the maximum number of students allowed.");
+						addStudent();
 					}
 					break;
 				case 5:
-					System.out.println("Do you want to add students to the Yoga class? (Y/N)");
-					yes = sc.nextLine().toLowerCase();
-					if (yes == "y" && yogaStudents <= 15) {
+					if (yogaStudents < 15) {
 						yogaStudents++;
+						System.out.println("Current students in the yoga class: " + yogaStudents);
+					} else {
+						System.out.println("Sorry, this class has reached the maximum number of students allowed.");
+						addStudent();
 					}
+					break;
+			}
+			if (choice < 0 || choice > 5) {
+				System.out.println(
+						"Invalid input! Enter one of the following options:\n'0' to quit this part of the program\n'1' to add students to Basketball Class\n'2' to add students to Swim Class\n'3' to add students to Ping Pong Class\n'4' to add students to Badminton Class\n'5' to add students to Yoga Class");
 			}
 		} while (choice != 0);
+
+		if (choice == 0) {
+			System.out.println("\nWe will now go back to the main menu.");
+			pickOption();
+		}
 	}
 
-	public static void schedule() {
+	public static void scheduling() {
 
-		// Main scheduling method
+		String[] classesShow = {"Basketball Class, Thursdays only, 1:00 to 2:00 PM, Maximum Students: 20",
+				"Swim Class, Fridays and Saturdays, 1:00 to 2:00 PM, Students: 10",
+				"Ping Pong Class, Mondays only, 9:00 to 10:00AM, Maximum Students: 10",
+				"Badminton Class, Mondays only, 1:00 to 2:00, Maximum Students: 12",
+				"Yoga Class, MRFS, 4:00 to 5:00 PM, Maximum Students: 15"};
+		String[] classesDisplay = {"Basketball\n" + basketballStudents + " out of 20 students", "Swim\n" + swimStudents + " out of 10 students", "Ping Pong\n" + pingPongStudents + " out of 10 students", "Badminton\n" + badmintonStudents + " out of 12 students", "Yoga\n" + yogaStudents + " out of 15 students"};
 
-		pickOption();
-		System.out.println("The first column is Monday, second Thursday, third Friday, and fourth Saturday.\n");
-		System.out.println("The first row is 9 to 10 AM, second 1 to 2 PM, third 3 to 4 PM, and fourth 4 to 5 PM");
-		printTable(table);
-		rowInput();
-		columnInput();
+		for (String i : classesDisplay) {
+			System.out.println(i);
+		}
 
+		int choice = 0;
+
+		System.out.println("The classes available to choose from are as follows:\n");
+		for (String i : classesShow) {
+			System.out.println("\t" + i);
+		}
+		System.out.println();
+		do {
+			System.out.println(
+					"Enter one of the following options:\n'0' to return to the main menu\n'1' to schedule the basketball class\n'2' to schedule the swim class\n'3' to schedule the ping pong class\n'4' to schedule the badminton class\n'5' to schedule the yoga class");
+			while (!sc.hasNextInt()) {
+				sc.nextLine();
+				System.out.println(
+						"Invalid input! Please enter '0' to return to the main menu\n'1' to schedule the basketball class\n'2' to schedule the swim class\n'3' to schedule the ping pong class\n'4' to schedule the badminton class\n'5' to schedule the yoga class");
+			}
+			switch (choice) {
+				case 1:
+					table[1][1] = classesDisplay[0];
+
+			}
+		} while (choice != 0);
+		if (choice == 0) {
+			System.out.println("\nWe will now go back to the main menu.");
+			pickOption();
+		}
 	}
+
 }
