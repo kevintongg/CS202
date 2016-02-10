a52868b5ca4ea3962567a396e0451bdd2b9ead01
Kevin Tong <kcr1230@hotmail.com>
2016-02-09 19:55:31 -0800

    Lab 4 Part 1

--- Lab 4 Part 1/lab4part1/PlayerMove.java
+++ Lab 4 Part 1/lab4part1/PlayerMove.java
@@ -15,199 +15,163 @@ import static lab4part1.GameBoard.getBoard;
 
 public class PlayerMove {
 
-	private static Turn player = Turn.RED;
-	private static boolean win = false;
-	private static int rows;
-	private static int columns;
-	private static Scanner sc = new Scanner(System.in);
-	private static ArrayList<PlayerMove> moves = new ArrayList<>();
+    private static Turn player = Turn.RED;
+    private static boolean win = false;
+    private static int rows;
+    private static int columns;
+    private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<PlayerMove> moves = new ArrayList<>();
 
-	public static void inputRow() {
+    public static void inputRow() {
 
-		do {
-			System.out.println("Enter the row index you would like to occupy (0 to 3): ");
-			typeValidation();
-			rows = sc.nextInt();
-			if (rows < 0 || rows > 3) {
-				System.out.println("A value between 0 and 3, please!");
-			}
-		} while (rows < 0 || rows > 3);
-	}
+        do {
+            System.out.println("Enter the row index you would like to occupy (0 to 3): ");
+            typeValidation();
+            rows = sc.nextInt();
+            if (rows < 0 || rows > 3) {
+                System.out.println("A value between 0 and 3, please!");
+            }
+        } while (rows < 0 || rows > 3);
+    }
 
-	public static void inputColumn() {
+    public static void inputColumn() {
 
-		do {
-			System.out.println("Enter the column index you would like to occupy (0 to 3): ");
-			typeValidation();
-			columns = sc.nextInt();
-			if (columns < 0 || columns > 3) {
-				System.out.println("A value between 0 and 3, please!");
-			}
-		} while (columns < 0 || columns > 3);
-	}
+        do {
+            System.out.println("Enter the column index you would like to occupy (0 to 3): ");
+            typeValidation();
+            columns = sc.nextInt();
+            if (columns < 0 || columns > 3) {
+                System.out.println("A value between 0 and 3, please!");
+            }
+        } while (columns < 0 || columns > 3);
+    }
 
-	public static boolean isWin() {
+    public static boolean isWin() {
 
-		return win;
-	}
+        return win;
+    }
 
-	public static void setWin(boolean win) {
-		PlayerMove.win = win;
+    public static void setWin(boolean win) {
+        PlayerMove.win = win;
 
-	}
+    }
 
-	public static int getRows() {
-		return rows;
-	}
+    public static int getRows() {
+        return rows;
+    }
 
-	public static void setRows(int rows) {
-		PlayerMove.rows = rows;
-	}
+    public static void setRows(int rows) {
+        PlayerMove.rows = rows;
+    }
 
-	public static int getColumns() {
-		return columns;
-	}
+    public static int getColumns() {
+        return columns;
+    }
 
-	public static void setColumns(int columns) {
-		PlayerMove.columns = columns;
-	}
+    public static void setColumns(int columns) {
+        PlayerMove.columns = columns;
+    }
 
-	public static void whosTurn() {
+    public static void whosTurn() {
 
-		if (player == Turn.RED) {
-			System.out.println();
-			System.out.println("Player 1's turn! (Red)");
-		} else if (player == Turn.BLACK) {
-			System.out.println();
-			System.out.println("Player 2's turn! (Black)");
-		}
-	}
+        if (player == Turn.RED) {
+            System.out.println();
+            System.out.println("Player 1's turn! (Red)");
+        } else if (player == Turn.BLACK) {
+            System.out.println();
+            System.out.println("Player 2's turn! (Black)");
+        }
+    }
 
-	public static void typeValidation() {
+    public static void typeValidation() {
 
-		while (!sc.hasNextInt()) {
-			sc.nextLine();
-			System.out.println("Invalid input! Please enter an integer value!");
-		}
-	}
+        while (!sc.hasNextInt()) {
+            sc.nextLine();
+            System.out.println("Invalid input! Please enter an integer value!");
+        }
+    }
 
-	public static void writeMovesToFile(ArrayList<PlayerMove> moves, String fileName) {
+    public static void writeMovesToFile(ArrayList<PlayerMove> moves, String fileName) {
 
-		Path path = Paths.get(fileName);
-		Charset charset = Charset.forName("UTF-8");
-		try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
-			for (PlayerMove move : moves) {
-				writer.write(move.toString());
-				writer.newLine();
-			}
-		} catch (IOException e) {
-			System.err.format("IOException %s%n", e);
-		}
-	}
+        Path path = Paths.get(fileName);
+        Charset charset = Charset.forName("UTF-8");
+        try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
+            for (PlayerMove move : moves) {
+                writer.write(move.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.format("IOException %s%n", e);
+        }
+    }
 
-	public static ArrayList<PlayerMove> getMoves() {
-		return moves;
-	}
+    public static ArrayList<PlayerMove> getMoves() {
+        return moves;
+    }
 
-	public static void setMoves(ArrayList<PlayerMove> moves) {
-		PlayerMove.moves = moves;
-	}
+    public static void setMoves(ArrayList<PlayerMove> moves) {
+        PlayerMove.moves = moves;
+    }
 
-	public static boolean checkWin() {
+    public static boolean checkWin() {
 
-		for (int i = 0; i < 16; i++) {
-			String piece = "Red";
-			if (player == Turn.RED) {
-				piece = "Black";
-			}
-			while (win == false) {
-				if (getBoard()[0][0].equals(piece) && getBoard()[0][1].equals(piece)
-						&& getBoard()[0][2].equals(piece) && getBoard()[0][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[1][0].equals(piece) && getBoard()[1][1].equals(piece)
-						&& getBoard()[1][2].equals(piece) && getBoard()[1][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[2][0].equals(piece) && getBoard()[2][1].equals(piece)
-						&& getBoard()[2][2].equals(piece) && getBoard()[2][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[3][0].equals(piece) && getBoard()[3][1].equals(piece)
-						&& getBoard()[3][2].equals(piece) && getBoard()[3][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[0][0].equals(piece) && getBoard()[1][0].equals(piece)
-						&& getBoard()[2][0].equals(piece) && getBoard()[3][0].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[0][1].equals(piece) && getBoard()[1][1].equals(piece)
-						&& getBoard()[2][1].equals(piece) && getBoard()[3][1].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[0][2].equals(piece) && getBoard()[1][2].equals(piece)
-						&& getBoard()[2][2].equals(piece) && getBoard()[3][2].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[0][3].equals(piece) && getBoard()[1][3].equals(piece)
-						&& getBoard()[2][3].equals(piece) && getBoard()[3][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[0][0].equals(piece) && getBoard()[1][1].equals(piece)
-						&& getBoard()[2][2].equals(piece) && getBoard()[3][3].equals(piece)) {
-					return win = true;
-				}
-				if (getBoard()[3][0].equals(piece) && getBoard()[2][1].equals(piece)
-						&& getBoard()[1][2].equals(piece) && getBoard()[0][3].equals(piece)) {
-					return win = true;
-				} else {
-					return win = false;
-				}
-			}
-		}
-		return win = false;
-	}
+        for (int i = 0; i < GameBoard.getBoard().length; i++) {
+            if (GameBoard.getBoard()[i].equals(CurrentTileToken.BLACK)) {
+                return win = true;
+            } else if (GameBoard.getBoard()[i].equals(CurrentTileToken.RED)) {
+                return win = true;
+            }
+            for (int j = 0; j < GameBoard.getBoard()[i].length; j++) {
+                if (GameBoard.getBoard()[j].equals(CurrentTileToken.BLACK)) {
+                    return win = true;
+                } else if (GameBoard.getBoard()[j].equals(CurrentTileToken.RED)) {
+                    return win = true;
+                }
+            }
+        }
+        return win = false;
+    }
 
-	public static void insertToken() {
+    public static void insertToken() {
 
-		if (player == Turn.RED) {
-			getBoard()[rows][columns].setTokenOnTile(CurrentTileToken.RED);
-		} else {
-			getBoard()[rows][columns].setTokenOnTile(CurrentTileToken.BLACK);
-		}
-	}
+        if (player == Turn.RED) {
+            getBoard()[rows][columns].setTokenOnTile(CurrentTileToken.RED);
+        } else {
+            getBoard()[rows][columns].setTokenOnTile(CurrentTileToken.BLACK);
+        }
+    }
 
-	public static void switchTurn() {
+    public static void switchTurn() {
 
-		if (player == Turn.RED) {
-			player = Turn.BLACK;
-		} else {
-			player = Turn.RED;
-		}
-	}
+        if (player == Turn.RED) {
+            player = Turn.BLACK;
+        } else {
+            player = Turn.RED;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "PlayerMove{" + "moves=" + moves + '}';
-	}
+    @Override
+    public String toString() {
+        return "PlayerMove{" + "moves=" + moves + '}';
+    }
 
-	public Scanner getSc() {
-		return sc;
-	}
+    public Scanner getSc() {
+        return sc;
+    }
 
-	public void setSc(Scanner sc) {
-		this.sc = sc;
-	}
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
 
-	public Turn getPlayer() {
-		return player;
-	}
+    public Turn getPlayer() {
+        return player;
+    }
 
-	public void setPlayer(Turn player) {
-		this.player = player;
-	}
+    public void setPlayer(Turn player) {
+        this.player = player;
+    }
 
-	public enum Turn {
-		RED, BLACK
-	}
+    public enum Turn {
+        RED, BLACK
+    }
 }
