8906024fd100ed1e7c07d8427019d0422b285bb5
Kevin Tong <kcr1230@hotmail.com>
2016-03-08 00:27:40 -0800

    zane

---
+++ Test/zane/arraydemo/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zane.arraydemo;
+
+/**
+ * @author John
+ */
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		arrayDemoForm arrayDemo = new arrayDemoForm();
+		arrayDemo.setVisible(true);
+	}
+
+}
